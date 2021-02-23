5be6687abe963c74fda95e8d124c52144be69503
Tanishka002 <75033184+Tanishka002@users.noreply.github.com>
2021-02-23 10:46:28 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -19,9 +19,27 @@ function setup(){
     hero = new Hero(200,200)
     fly = new Fly(hero.body,{x:300, y:50})
     ground = new Ground(600,300,1200,50);
-   
-
-   
+    box1 = new Box(500,400,70,70);
+    box2 = new Box(500,400,70,70);
+    box3 = new Box(500,400,70,70);
+    box4 = new Box(500,400,70,70);
+    box5 = new Box(500,400,70,70);
+    box6 = new Box(500,400,70,70);
+    box7 = new Box(500,400,70,70);
+    box8 = new Box(900,400,70,70);
+    box9 = new Box(900,400,70,70);
+    box10 = new Box(900,400,70,70);
+    box11 = new Box(900,400,70,70);
+    box12 = new Box(900,400,70,70);
+    box13 = new Box(900,400,70,70);
+    box14 = new Box(650,400,70,70);
+    box15 = new Box(650,400,70,70);
+    box16 = new Box(650,400,70,70);
+    box17 = new Box(650,400,70,70);
+    box18 = new Box(650,400,70,70);
+    box19 = new Box(650,400,70,70);
+    box20 = new Box(650,400,70,70);
+    monster = new Monster (1000,400,25)
 }
 
 function draw(){
@@ -31,6 +49,27 @@ function draw(){
    hero.display();   
    ground.display();
    fly.display();
+   box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+    box8.display();
+    box9.display();
+    box10.display(); 
+    box11.display();
+    box12.display();
+    box13.display();
+    box14.display();
+    box15.display();
+    box16.display();
+    box17.display();
+    box18.display();
+    box19.display();
+    box20.display();
+    monster.display(); 
 }
 
 function mouseDragged(){
